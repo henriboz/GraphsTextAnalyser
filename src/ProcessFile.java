eb3888df7128b5b79c362cdc4c099ad929895ba0
Henrique Boz <henrique.boz@arris.com>
2018-11-26 16:22:47 -0200

    Adding new words to the regex expression of discarded words

--- src/ProcessFile.java
+++ src/ProcessFile.java
@@ -11,7 +11,7 @@ public final class ProcessFile extends SimpleFileVisitor<Path> {
     private String currentFileContent = null;
     private String currentFilePath = null;
     private Graph graph;
-    private String stopWords = "\\b( ? 'ourselves|hers|between|yourself|but|again|there|about|once|during|out|very|having|with|they|own|an|be|some|for|do|its|yours|such|into|of|most|itself|other|off|is|s|am|or|who|as|from|him|each|the|themselves|until|below|are|we|these|your|his|through|don|nor|me|were|her|more|himself|this|down|should|our|their|while|above|both|up|to|ours|had|she|all|no|when|at|any|before|them|same|and|been|have|in|will|on|does|yourselves|then|that|because|what|over|why|so|can|did|not|now|under|he|you|herself|has|just|where|too|only|myself|which|those|i|after|few|whom|t|being|if|theirs|my|against|a|by|doing|it|how|further|was|here|than)\\b";
+    private String stopWords = "\\b( ? '|could|always|every|ever|think|nothin|shall|say|see|great|good|soon|though|said|one|know|little|never|well|without|would|must|much|might|may|ourselves|hers|between|yourself|but|again|there|about|once|during|out|very|having|with|they|own|an|be|some|for|do|its|yours|such|into|of|most|itself|other|off|is|s|am|or|who|as|from|him|each|the|themselves|until|below|are|we|these|your|his|through|don|nor|me|were|her|more|himself|this|down|should|our|their|while|above|both|up|to|ours|had|she|all|no|when|at|any|before|them|same|and|been|have|in|will|on|does|yourselves|then|that|because|what|over|why|so|can|did|not|now|under|he|you|herself|has|just|where|too|only|myself|which|those|i|after|few|whom|t|being|if|theirs|my|against|a|by|doing|it|how|further|was|here|than)\\b";
     public ProcessFile(Graph graph){
         this.graph = graph;
     }
